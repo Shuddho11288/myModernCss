064727349e2f19b5556ccc561132308307321b3d
Shuddho11288 <76694476+Shuddho11288@users.noreply.github.com>
2021-10-08 16:33:34 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-function scrollDown(){
-    var elmnt = document.getElementById("download-element");
-    elmnt.scrollIntoView();
-}
+
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
   }
